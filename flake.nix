72eb3169ea1aee18953cb67fbe0019d74b3d922e
sandorex <rzhw3h@gmail.com>
2025-05-16 18:20:37 +0200

    Add simple nix flake for devshell x86_64
    This is the start, i plan to make the CI use nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit nixpkgs;
+
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.linux = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          ninja
+          just
+          cmake
+          validatePkgConfig
+          gcc
+          wayland-scanner
+          zenity
+          libffi
+          python313
+        ];
+
+        # runtime dependencies
+        buildInputs = with pkgs; [
+          vulkan-headers
+          vulkan-loader
+          libGL
+          libusb1
+          libayatana-appindicator
+          libdrm
+          mesa
+          wayland
+          wayland-protocols
+          pipewire
+          libpulseaudio
+          alsa-lib
+          dbus
+          libxkbcommon
+          xorg.libX11
+          xorg.libXScrnSaver
+          xorg.libXcursor
+          xorg.libXext
+          xorg.libXfixes
+          xorg.libXi
+          xorg.libXrandr
+        ];
+
+        # use Ninja generator by default
+        CMAKE_GENERATOR = "Ninja";
+      };
+    };
+}
